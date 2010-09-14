0bb651006b41e2e8951a5cdf252e3325be96d254
Jonathan Chu <jonathan.chu@me.com>
2010-09-13 22:00:50 -0400

    require linum.el and set custom formatting for line numbers like textmate

--- jontourage/global.el
+++ jontourage/global.el
@@ -44,3 +44,7 @@
 ;; linum mode
 (require 'linum)
 (global-linum-mode 1)
+(setq linum-format 
+    (lambda (line) (propertize 
+        (format (let ((w (length (number-to-string (count-lines (point-min) (point-max)))))) 
+            (concat " %" (number-to-string w) "d ")) line) 'face 'linum)))
