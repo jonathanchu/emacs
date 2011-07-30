2b95bed94c691c8521b52e89e2a96b5208f02492
Jonathan Chu <jonathan@Jonathans-MacBook-Air-2.local>
2011-07-29 23:54:10 -0400

    comment out like a pro

--- emacs.el
+++ emacs.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d")
 
 (add-to-list 'exec-path "/usr/local/bin")
-# (setq magit-git-executable "/usr/local/bin/git")
+;; (setq magit-git-executable "/usr/local/bin/git")
 (setq magit-git-executable "/usr/bin/git")
 
 (load "local")
