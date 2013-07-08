963d779b14e8c630687253037c6325474d3a5884
Jonathan Chu <jonathan.chu@me.com>
2013-07-08 01:30:24 -0400

    mobileorg is a pain. finally got the config right...

--- jontourage/org_mode.el
+++ jontourage/org_mode.el
@@ -1,11 +1,8 @@
-(require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
 ;; mobileorg settings
 (setq org-directory "~/org")
+(setq org-mobile-inbox-for-pull "~/org/inbox.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
 
 (setq org-log-done t)
 
-(setq org-agenda-files (list "~/org/projects.org"))
+(setq org-mobile-files '("~/org"))
