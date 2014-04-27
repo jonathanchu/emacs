0d2a58611619e2da6d1e15bbde2bd2492c0c90fa
Jonathan Chu <jonathan.chu@me.com>
2014-04-27 02:00:48 -0400

    Remove a lot of unused packages and config code.

--- jontourage/org_mode.el
+++
@@ -1,8 +0,0 @@
-;; mobileorg settings
-(setq org-directory "~/Dropbox/org")
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/inbox.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-
-(setq org-log-done t)
-
-(setq org-mobile-files '("~/Dropbox/org"))
--- jontourage/yasnippet.el
+++
@@ -1,8 +0,0 @@
-; yasnippet
-; (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet-0.6.1c")
-(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.8.0")
-(require 'yasnippet)
-; (yas/initialize)
-; (yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
-; (yas/load-directory "~/.emacs.d/elpa/yasnippet-0.8.0/snippets")
-(yas-global-mode 1)
