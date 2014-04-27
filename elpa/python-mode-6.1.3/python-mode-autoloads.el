2eae3bbd2becf85eee11f6746512a554bc2c81c4
Jonathan Chu <jonathan.chu@me.com>
2014-04-27 02:09:40 -0400

    Add back in python-mode to fix indention stuff.

---
+++ elpa/python-mode-6.1.3/python-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; python-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
+;;;;;;  (21340 34809 730669 0))
+
+;;;***
+
+(provide 'python-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; python-mode-autoloads.el ends here
