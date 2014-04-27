879e731cc28151af00affe1eb0de6b531bd2234f
Jonathan Chu <jonathan.chu@me.com>
2014-04-27 02:09:56 -0400

    Add highlight linum mode package.

---
+++ elpa/hlinum-20140107.220/hlinum-autoloads.el
@@ -0,0 +1,33 @@
+;;; hlinum-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (hlinum-deactivate hlinum-activate) "hlinum" "hlinum.el"
+;;;;;;  (21339 8860 0 0))
+;;; Generated autoloads from hlinum.el
+
+(autoload 'hlinum-activate "hlinum" "\
+Enable highlighting current line number.
+
+\(fn)" t nil)
+
+(autoload 'hlinum-deactivate "hlinum" "\
+Disable highlighting current line number.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("hlinum-pkg.el") (21339 8860 941131 0))
+
+;;;***
+
+(provide 'hlinum-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hlinum-autoloads.el ends here
---
+++ elpa/hlinum-20140107.220/hlinum-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hlinum" "20140107.220" "Extension for linum.el to highlight current line number" (quote ((cl-lib "0.2"))))
