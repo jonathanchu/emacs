65e9982c711b6d8751dca1c96bba1d85806868b3
Jonathan Chu <jonathan.chu@me.com>
2014-04-27 02:05:41 -0400

    Get the right python specific code in here. DOH!

--- jontourage/python.el
+++ jontourage/python.el
@@ -1,19 +1,35 @@
-(defun coffee-custom ()
-  "coffee-mode-hook"
+(add-hook 'python-mode-hook
+          (lambda ()
+            (define-key python-mode-map "\C-m"
+              'python-reindent-then-newline-and-indent)
+             (add-hook 'local-write-file-hooks
+                        '(lambda()
+                           (save-excursion
+                             (untabify (point-min) (point-max))
+                             (delete-trailing-whitespace))))
+             (set (make-local-variable 'tab-width) 4)
+             (set (make-local-variable 'indent-tabs-mode) 'nil)))
 
-  ;; CoffeeScript uses two spaces.
-  (make-local-variable 'tab-width)
-  (set 'tab-width 2)
+(defun python-reindent-then-newline-and-indent ()
+ "Reindents the current line then creates an indented newline."
+ (interactive "*")
+ (newline)
+ (save-excursion
+   (end-of-line 0)
+   (indent-according-to-mode)
+   (delete-region (point) (progn (skip-chars-backward " \t") (point))))
+ (when (python-previous-line-is-comment)
+     (insert "# "))
+ (indent-according-to-mode))
 
-  ;; If you don't want your compiled files to be wrapped
-  (setq coffee-args-compile '("-c" "--bare"))
+(defun python-previous-line-is-comment ()
+ "Returns `t' if the previous line is a Python comment."
+ (save-excursion
+   (forward-line -1)
+   (python-line-is-comment)))
 
-  ;; Emacs key binding
-  (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
-
-  ;; Compile '.coffee' files on every save
-  (and (file-exists-p (buffer-file-name))
-       (file-exists-p (coffee-compiled-file-name))
-       (coffee-cos-mode t)))
-
-(add-hook 'coffee-mode-hook 'coffee-custom)
+(defun python-line-is-comment ()
+ "Returns `t' if the current line is a Python comment."
+ (save-excursion
+   (beginning-of-line)
+   (search-forward "#" (point-at-eol) t)))
