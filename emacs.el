5dc243e6040868cf926218b479db47d92a69018b
Jonathan Chu <jonathan.chu@me.com>
2014-08-17 23:04:33 -0400

    Stuff from installing packages. Shouldn't touch this.

--- emacs.el
+++ emacs.el
@@ -3,3 +3,29 @@
 (setq magit-git-executable "/usr/bin/git")
 
 (load "local")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2d2d2d" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#6699cc" "#cccccc"])
+ '(ansi-term-color-vector [unspecified "#2d2d2d" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#6699cc" "#cccccc"])
+ '(background-color "#042028")
+ '(background-mode dark)
+ '(cursor-color "#708183")
+ '(custom-safe-themes (quote ("cf2bb5e8046ca363183c87e8d33932f2a76a3d705b9db2721631777bbce92968" "97eed4405ea27b7142b5ce26b4fa475551956793d1c5fa7c5a62f297180aadd7" "e992575f7c09459bfc190e6776b8f5f96964023e98267a87fb3094e7c9686776" "9117c98819cfdeb59780cb43e5d360ff8a5964d7dd9783b01708bda83098b9fd" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "007a1e734c76cf40e203fd9d33b51b83a41b0dfe645d5b5fa34ef6e4f0e5da0b" "3b1973119d3ca55a31abffd64cf6b7d12ce9de125ae07574dd43b90c5f9d5896" "12fece7b8ef725588213723b85b02bc7610b8cdee26dc48b32c12364a04088d2" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "405fda54905200f202dd2e6ccbf94c1b7cc1312671894bc8eca7e6ec9e8a41a2" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" default)))
+ '(foreground-color "#708183"))
+;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;'(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+; '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:foreground "#030303" :background "#4182c4" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
