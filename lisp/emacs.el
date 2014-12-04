a1fbe161e6b29f19dcf21c6650ed7d9862e27fd5
Jonathan Chu <jonathan.chu@me.com>
2014-12-03 23:48:13 -0500

    Don't load `local` here.
    I'm doing this somewhere else now.

--- lisp/emacs.el
+++ lisp/emacs.el
@@ -2,7 +2,7 @@
 
 (setq magit-git-executable "/usr/bin/git")
 
-(load "local")
+;; (load "local")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
