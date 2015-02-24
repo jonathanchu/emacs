2af349d5d694f6c4cc653f7f0752789590271aa6
Jonathan Chu <jonathan.chu@me.com>
2015-02-24 15:42:40 -0500

    Update packages.

--- elpa/python-mode-6.1.3/python-mode-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; python-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (21652 28351 532625 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; python-mode-autoloads.el ends here
